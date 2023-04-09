0a8398da799b1dbfe18019be9f3f6e03502390c4
AviX4 <56990359+AviX4@users.noreply.github.com>
2023-04-09 21:56:17 +0530

    Added Insertion operation to map

--- map.js
+++ map.js
@@ -9,3 +9,50 @@ class linked_list{
         this.head = head;
     }
 }
+
+let buffer = 7;
+let arr =[] ;
+for(let i=0;i<7;i++){
+    let h = new Node(-1);
+    let l = new linked_list(h);
+    arr.push(l);
+}
+
+function traverse(){
+    arr.forEach(function(item,index){
+        console.log(item);
+    })
+}
+
+//Modify insertion code to make it cleaner
+function insertion(value){
+    let idx = value%buffer;
+    if(!arr[idx].head.next){
+        let lx = new Node(value);
+        arr[idx].head.next = lx;
+    }else{
+        let trav = arr[idx];
+        trav = trav.head.next;
+        while(trav.next){
+            trav = trav.next;
+        }
+        let lx = new Node(value);
+        trav.next = lx;
+    }
+    return;
+}
+
+
+
+
+// let node1 = new Node(2);
+// let node2 = new Node(3);
+// node1.next = node2;
+// let l1 = new linked_list(node1);
+// let trav = l1.head;
+// while(trav){
+//     console.log(trav);
+//     trav++;
+// // }
+// Node { val: 2, next: Node { val: 3, next: null } }
+// ^ how a linked list would look like in js
