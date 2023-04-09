578404b9a04e3d97e79cb5ade360be56f9a44c79
AviX4 <56990359+AviX4@users.noreply.github.com>
2023-04-09 13:08:04 +0530

    Initial commit

---
+++ map.js
@@ -0,0 +1,11 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+class linked_list{
+    constructor(head = null){
+        this.head = head;
+    }
+}
